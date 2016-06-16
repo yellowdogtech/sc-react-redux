1ec0df319b46b0655cfe92077692d283781035a9
Derrick Brown <derrick.brown@w-dn-ml-dbrown.local>
2016-06-16 15:58:05 -0600

    progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('My App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
+import Stream from './components/Stream';
+
+const tracks = [
+    {
+        title: 'Some track'
+    },
+    {
+        title: 'Some other track'
+    }
+];
+
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
+ReactDOM.render(
+    <Stream />,
+    document.getElementById('app')
+);
